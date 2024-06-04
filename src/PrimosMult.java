5e15360a4798f174271ca5dc0e5ee51a6a0aebe9
Gustavo Passos <gusapas26@gmail.com>
2024-06-04 19:45:58 -0300

    primosmuilt

--- src/PrimosMult.java
+++ src/PrimosMult.java
@@ -1,23 +1,46 @@
+import java.util.Scanner;
+
 public class PrimosMult {
     //Múltiplas threads para subintervalos
     public static void main(String[] args) {
-        int inicio = 5;
-        int fim = 10000000;
-        //int threads = 4;
-        int intervalo = 2500;
+        int inicio = 2;
+        int fim = 1000000;
+        System.out.println("Digite quantas threads: ");
+        int qnt_threads = new Scanner(System.in).nextInt();
 
-        for (int i = inicio; i <= fim; i += intervalo) {
-            int fimSubintervalo = Math.min(i + intervalo - 1, fim);
-            int finalI = i;
-            Thread thread = new Thread(() -> imprimirPrimos(finalI, fimSubintervalo));
-            thread.start();
+        System.out.println("Digite o intervalo: ");
+        int intervalo = new Scanner(System.in).nextInt();
+
+        class th{
+
+            String nome;
+            public th(String nome){
+                this.nome = nome;
+            }
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = inicio; i <= fim; i += intervalo) {
+                        int fimSubintervalo = Math.min(i + intervalo - 1, fim);
+                        int finalI = i;
+                        imprimirPrimos(finalI, fimSubintervalo, nome);
+
+                    }
+                }
+            });
+        }
+
+        for (int i = 0; i<qnt_threads; i++){
+            th t = new th(String.format("%d", i+1));
+            t.thread.start();
         }
     }
 
-    private static void imprimirPrimos(int inicio, int fim) {
+    private static void imprimirPrimos(int inicio, int fim, String nome) {
         for (int i = inicio; i <= fim; i++) {
             if (ehPrimo(i)) {
-                System.out.println(i);
+                System.out.println(i + String.format(" - T%s", nome));
             }
         }
     }
