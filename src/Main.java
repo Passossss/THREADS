db46ff6b9c4d88f7128d8c77aae65c2fe5e24693
Gustavo Passos <gusapas26@gmail.com>
2024-06-04 14:16:20 -0300

    ex1

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,37 @@ public class Main {
 
     public static void main(String[] args) {
         int intervaloInicio = 2;
-        int intervaloFim = 10;
-        //ex 1 - 2;10
-
+        int intervaloFim = 100;
+        //ex1 2,10 usar
 
         List<Integer> primosEncontrados = new ArrayList<>();
 
-        int numeroThreads = 4;
-        int tamanhoSubintervalo = (intervaloFim - intervaloInicio) / numeroThreads;
+       // int numeroSubintervalos = 4;
+       // int tamanhoSubintervalo = (intervaloFim - intervaloInicio) / numeroSubintervalos;
 
-        List<MinhaThread> threadsCriadas = new ArrayList<>(); // Lista para armazenar os objetos MinhaThread
-
-        for (int i = 0; i < numeroThreads; i++) {
+        List<MinhaThread> threadsCriadas = new ArrayList<>();
+        //ex D)
+        /*for (int i = 0; i < numeroSubintervalos; i++) {
             int inicioSubintervalo = intervaloInicio + i * tamanhoSubintervalo;
             int fimSubintervalo = Math.min(inicioSubintervalo + tamanhoSubintervalo - 1, intervaloFim);
             MinhaThread thread = new MinhaThread(inicioSubintervalo, fimSubintervalo, primosEncontrados);
             thread.start();
             threadsCriadas.add(thread);
-        }
+        }*/
 
-        for (MinhaThread thread : threadsCriadas) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        // para exercicio C) int metade = (intervaloFim - intervaloInicio) / 2;
+
+        MinhaThread thread1 = new MinhaThread(intervaloInicio, metade, primosEncontrados);
+        thread1.start();
+
+        MinhaThread thread2 = new MinhaThread(metade + 1, intervaloFim, primosEncontrados);
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         System.out.println("Primos encontrados no intervalo [" + intervaloInicio + "," + intervaloFim + "]:");
--- src/MinhaThread.java
+++ src/MinhaThread.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class MinhaThread {
+import java.util.List;
+
+class MinhaThread extends Thread {
+
+    private final int inicio;
+    private final int fim;
+    private final List<Integer> primosEncontrados;
+
+    public MinhaThread(int inicio, int fim, List<Integer> primosEncontrados) {
+        this.inicio = inicio;
+        this.fim = fim;
+        this.primosEncontrados = primosEncontrados;
+    }
+
+    @Override
+    public void run() {
+        for (int numero = inicio; numero <= fim; numero++) {
+            if (ehPrimo(numero)) {
+                primosEncontrados.add(numero);
+            }
+        }
+    }
+
+    private boolean ehPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
