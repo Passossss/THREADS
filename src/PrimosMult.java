cd81faad31a10314e7b3501eeb4bae09f15bc04a
Gustavo Passos <gusapas26@gmail.com>
2024-06-07 11:32:54 -0300

    ex1

--- src/PrimosMult.java
+++ src/PrimosMult.java
@@ -3,59 +3,69 @@ import java.util.Scanner;
 public class PrimosMult {
     //Múltiplas threads para subintervalos
     public static void main(String[] args) {
-        int inicio = 2;
-        int fim = 1000000;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o intervalo de início: ");
+        int inicio = scanner.nextInt();
+
+        System.out.println("Digite o intervalo de fim: ");
+        int fim = scanner.nextInt();
+
         System.out.println("Digite quantas threads: ");
-        int qnt_threads = new Scanner(System.in).nextInt();
+        int qnt_threads = scanner.nextInt();
 
-        System.out.println("Digite o intervalo: ");
-        int intervalo = new Scanner(System.in).nextInt();
+        int intervalo = (fim - inicio + 1) / qnt_threads;
 
-        class th{
+        class PrimeThread extends Thread {
+            private int threadId;
+            private int start;
+            private int end;
 
-            String nome;
-            public th(String nome){
-                this.nome = nome;
+            public PrimeThread(int threadId, int start, int end) {
+                this.threadId = threadId;
+                this.start = start;
+                this.end = end;
             }
 
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for (int i = inicio; i <= fim; i += intervalo) {
-                        int fimSubintervalo = Math.min(i + intervalo - 1, fim);
-                        int finalI = i;
-                        imprimirPrimos(finalI, fimSubintervalo, nome);
-
+            @Override
+            public void run() {
+                for (int i = start; i <= end; i++) {
+                    if (ehPrimo(i)) {
+                        System.out.println(i + " T" + threadId);
                     }
                 }
-            });
-        }
-
-        for (int i = 0; i<qnt_threads; i++){
-            th t = new th(String.format("%d", i+1));
-            t.thread.start();
-        }
-    }
-
-    private static void imprimirPrimos(int inicio, int fim, String nome) {
-        for (int i = inicio; i <= fim; i++) {
-            if (ehPrimo(i)) {
-                System.out.println(i + String.format(" - T%s", nome));
             }
         }
+
+        Thread[] threads = new Thread[qnt_threads];
+
+        for (int i = 0; i < qnt_threads; i++) {
+            int start = inicio + i * intervalo;
+            int end = (i == qnt_threads - 1) ? fim : (start + intervalo - 1);
+            threads[i] = new PrimeThread(i + 1, start, end);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < qnt_threads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        scanner.close();
     }
 
     private static boolean ehPrimo(int numero) {
         if (numero <= 1) {
             return false;
         }
-
         for (int i = 2; i <= Math.sqrt(numero); i++) {
             if (numero % i == 0) {
                 return false;
             }
         }
-
         return true;
     }
 }
